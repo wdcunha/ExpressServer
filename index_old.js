f1c059be83cc6a907884ee2081d1eeaf24b6aa0a
Wellington <wdcunha@gmail.com>
2017-11-30 12:43:00 -0800

    created contact us form and thank you page

--- index_old.js
+++ index_old.js
@@ -1,3 +1,5 @@
+// *** this is the minimal that is needed to run a web server with Express
+
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -5,46 +7,50 @@ const bodyParser = require('body-parser');
 // When we require `express`, we get function that we can call
 // to create an "instance of" `express` app. We will use methods
 // of the app object to build our web server.
-const app = express();
+const app = express(); //creates an instance of Express
 
 // ð Configures our application to use `ejs` as the templating engine
 // to render our applications HTML pages. The package, ejs, must still
 // be installed with npm for this to work.
 app.set('view engine', 'ejs');
 
-app.use(morgan('dev'));
+
+// it's a high order function that runs middleware that is a function
+app.use(morgan('dev')); // we will use this because the simplest information that it gives
+// app.use(morgan('combined')); // much more information
+
 // bodyParser.urlencoded middleware will allow our application to parse
 // data coming from an HTML form into a JavaScript object that will
-// be available as a property of the request object. That property is named
-// `body`, `request.body`.
+// be available as a property of the request object.
 app.use(bodyParser.urlencoded({extended: false}));
 
-/*
+/*  ///// >>>>> as we added the line above, this peace of code would be duplicated <<<<
+
+// to use next function from middleware, it's necessary to write this line:
 app.use((request, response, next) => {
   console.log(`${request.method} - ${request.path} - ${new Date().toString()}`);
   // The `next` argument is a function that when called tells express
-  // that the middleware is finished and its time to move to the next
-  // one in line. If you forget to call `next`, your client will appear
-  // to load forever while it waits for a response.
-  next();
+ // that the middleware is finished and its time to move to the next
+ // one in line. If you forget to call `next`, your client will appear
+ // to load forever while it waits for a response.
+  next();  //if there's no this line, the browser won't finish refreshing
 });
 */
 
-// The `request` represents a HTTP request. It contains
-// information about the request including the verb, the headers
-// any data, etc. The `request` is sent by the client usually
-// a browser.
 
-// The `response` represents a HTTP response. It's object
-// that contains a header & a body of data that our application
-// will built to reply to the client's `request`.
+// when in the URL call /home, this get will run
+// the names doesn't matter, but order yes
+// The `request` represents a HTTP request. It contains information about the request including the verb,
+// the headers any data, etc. The `request` is sent by the client usually a browser.
+// The `response` represents a HTTP response. It's object that contains a header & a body of data that our
+// application will built to reply to the client's `request`.
+// one request give, one response will be given
 app.get('/home', (request, response) => {
-  /*
+/*
   response.send(`
     Convergence user centered design disrupt pitch.
   `);
   */
-
   // `response.render` is used instead of `response.send`
   // to serve the contents of a template in a response.
   // It finalizes the response.
@@ -60,17 +66,12 @@ app.get('/contact_us', (request, response) => {
   response.render('contactUs');
 });
 
+
 app.post('/contact_us', (request, response) => {
   const body = request.body;
-  const fullName = body.fullName;
-  const message = body.message;
-  // response.send(body);
-
-  // `response.render` can take an object as a second argument.
-  // All properties of this object will act as local variables inside
-  // of the template being rendered.
-  response.render('thankYou', {fullName: fullName, message: message});
+  response.send(body);
 });
+
 
 const DOMAIN = 'localhost';
 const PORT = '3002';
