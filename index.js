0bc8b3003d568a02dea75f2fc4db24528513be82
Wellington <wdcunha@gmail.com>
2017-11-30 12:06:17 -0800

    Install ejs, set it up and serve home.ejs

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ const morgan = require('morgan');
 // of the app object to build our web server.
 const app = express(); //creates an instance of Express
 
+// ð Configures our application to use `ejs` as the templating engine
+// to render our applications HTML pages. The package, ejs, must still
+// be installed with npm for this to work.
+app.set('view engine', 'ejs');
+
+
 // it's a high order function that runs middleware that is a function
 app.use(morgan('dev')); // we will use this because the simplest information that it gives
 // app.use(morgan('combined')); // much more information
 
-/*  ///// as we added the line above, this peace of code would be duplicated
+/*  ///// >>>>> as we added the line above, this peace of code would be duplicated <<<<
 
 // to use next function from middleware, it's necessary to write this line:
 app.use((request, response, next) => {
@@ -34,12 +40,29 @@ app.use((request, response, next) => {
 // application will built to reply to the client's `request`.
 // one request give, one response will be given
 app.get('/home', (request, response) => {
+/*
   response.send(`
     Convergence user centered design disrupt pitch deck food-truck workflow experiential fund workflow grok convergence.
     Steve Jobs big data thinker-maker-doer responsive experiential responsive SpaceTeam piverate engaging pivot long shadow pitch deck.
     User centered design human-centered design disrupt disrupt fund affordances integrate paradigm integrate workflow affordances.
     Iterate grok food-truck quantitative vs. qualitative big data engaging hacker.
   `);
+
+  /*
+  response.send(`
+    Convergence user centered design disrupt pitch.
+  `);
+  */
+
+  // `response.render` is used instead of `response.send`
+  // to serve the contents of a template in a response.
+  // It finalizes the response.
+
+  // The first argument to `response.render` is the file path
+  // (not the URL path) to the template we want to render
+  // beginning from the `/views` subdirectory without the
+  // file extension (i.e. no `.ejs`).
+  response.render('home');
 });
 
 const DOMAIN = 'localhost';
